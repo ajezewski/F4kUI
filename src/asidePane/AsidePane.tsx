dc2a727c3886afadac782c9703141b43eddd0cf8
Adam <kontakt@wiweb.pl>
2019-01-22 19:35:43 +0100

    Add .editorconfig

--- src/asidePane/AsidePane.tsx
+++ src/asidePane/AsidePane.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import './asidePane.css';
 
 class AsidePane extends React.Component {
-    private expandWidth: string;
-    private position: string;
-    private style: object = {};
+  private expandWidth: string;
+  private position: string;
+  private style: object = {};
 
-    constructor(props: any) {
-        super(props);
-        this.expandWidth = props.expandWidth;
-        this.position = props.position;
-    }
+  constructor(props: any) {
+    super(props);
+    this.expandWidth = props.expandWidth;
+    this.position = props.position;
+  }
 
-    handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    }
+  handleClick = (e: React.MouseEvent<HTMLElement>) => {
+  };
 
-    render() {
-       return (
-           <aside onClick={ this.handleClick } className={ this.position }>
-           </aside>
-       )
-    }
+  render() {
+    return (
+      <aside onClick={this.handleClick} className={this.position}>
+      </aside>
+    )
+  }
 }
 
 export default AsidePane;
--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './button.css';
 
-const Button = ({ btnType, className, value } = {btnType: '', className: '', value: ''}) => {
-    return (
-        <button
-            type={ btnType }
-            className={ className }
-        ><span>{ value }</span></button>
-    )
-}
+const Button = ({btnType, className, value} = {btnType: '', className: '', value: ''}) => {
+  return (
+    <button
+      type={btnType}
+      className={className}
+    >
+      <span>{value}</span>
+    </button>
+  )
+};
 
 export default Button;
